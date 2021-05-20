d49c7297ed4898ad3465bd5f1fb315e0b260aff1
camillaxyc <camillachen97@gmail.com>
2021-05-19 23:20:01 -0700

    import Image to layout

--- components/layout.js
+++ components/layout.js
@@ -1,5 +1,6 @@
 import styles from './layout.module.css'
 import utilStyles from '../styles/utils.module.css'
+import Image from 'next/image'
 
 export default function Layout({children}){
     return <div className={styles.container}>{children}</div>
