2ce202fb84fd74046306625b39d756e230d9fb77
camillaxyc <camillachen97@gmail.com>
2021-05-19 23:23:48 -0700

    add name const to layout

--- components/layout.js
+++ components/layout.js
@@ -2,7 +2,11 @@ import styles from './layout.module.css'
 import utilStyles from '../styles/utils.module.css'
 import Image from 'next/image'
 import Head from 'next/head'
+import Link from 'next/link'
 
+
+const name = 'Camilla Chen'
+export const siteTitle = 'Next.js Sample Website'
 export default function Layout({children}){
     return <div className={styles.container}>{children}</div>
 }
