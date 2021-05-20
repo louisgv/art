b906d6a8828544266ddf2565ad9f94695afef3fa
camillaxyc <camillachen97@gmail.com>
2021-05-19 23:21:15 -0700

    import Head to layout.js

--- components/layout.js
+++ components/layout.js
@@ -1,6 +1,7 @@
 import styles from './layout.module.css'
 import utilStyles from '../styles/utils.module.css'
 import Image from 'next/image'
+import Head from 'next/head'
 
 export default function Layout({children}){
     return <div className={styles.container}>{children}</div>
