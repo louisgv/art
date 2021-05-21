7cfbd7ac61089de36e76a8fb363a24ba2e1036e2
camillaxyc <camillachen97@gmail.com>
2021-05-20 23:57:37 -0700

    added words to paragraph

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -20,6 +20,7 @@ export default function FirstPost() {
       </p>
       <p>
         Nothing to see here. I am trying to get to 25 commit before 11:59PM.
+        ABCDEFG
       </p>
       <h2>
         Why it's important to not procrastinate:
