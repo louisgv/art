0dad93b0ab4666c116bdf075be48f0f83b85bb77
camillaxyc <camillachen97@gmail.com>
2021-05-20 23:48:53 -0700

    added link and import Link to index.js

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import Layout, { siteTitle } from '../components/layout'
 import utilStyles from '../styles/utils.module.css'
+import Link from 'next/link'
 
 export default function Home() {
   return (
@@ -15,6 +16,13 @@ export default function Home() {
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
         </p>
       </section>
+      <h1 className="title">
+        Go to{' '}
+        <Link href="/posts/first-post">
+        <a>this page!</a>
+  </Link>
+</h1>
+
     </Layout>
   )
 }
