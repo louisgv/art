e7b086df56da8a83035a504c900e8f386df69700
Louis <6723574+louisgv@users.noreply.github.com>
2021-05-23 03:24:33 -0400

    just add some beautifier

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
-import Head from 'next/head'
-import Layout, { siteTitle } from '../components/layout'
-import utilStyles from '../styles/utils.module.css'
-import Link from 'next/link'
+import Head from "next/head";
+import Layout, { siteTitle } from "../components/layout";
+import utilStyles from "../styles/utils.module.css";
+import Link from "next/link";
 
 export default function Home() {
   return (
@@ -10,22 +10,21 @@ export default function Home() {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>Hello, I'm Camilla. I'm passionate about Web Development and Geogprahic Information Systems.
-          I love coding in Javascript and Java.
-        </p>
-        
         <p>
-          (This is a sample website - you’ll be building a site like this on{' '}
+          Hello, I'm Camilla. I'm passionate about Web Development and
+          Geogprahic Information Systems. I love coding in Javascript and Java.
+        </p>
+        <p>
+          (This is a sample website - you’ll be building a site like this on{" "}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
         </p>
       </section>
       <h1 className="title">
-        Go to{' '}
+        Go to{" "}
         <Link href="/posts/first-post">
-        <a>this page!</a>
-  </Link>
-</h1>
-
+          <a>this page!</a>
+        </Link>
+      </h1>
     </Layout>
-  )
+  );
 }
