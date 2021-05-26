075b4febb1690eb74c5871c3cc53b55f1e210b2c
camillaxyc <camillachen97@gmail.com>
2021-05-25 22:42:07 -0700

    added function to id js

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -3,3 +3,7 @@ import layout from '../../components/layout'
 export default function Post() {
     return <Layout>...</Layout>
 }
+
+export async function getStaticPaths() {
+    // Return a list of possible value for id
+}
