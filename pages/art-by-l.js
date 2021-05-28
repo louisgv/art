b8e32fd0306440d77f8f0cd36cccb397d5590957
Karlos Santos <6723574+louisgv@users.noreply.github.com>
2021-05-27 23:11:15 -0400

    Add signature

--- pages/art-by-l.js
+++ pages/art-by-l.js
@@ -20,11 +20,21 @@ export default function ArtByL() {
           class={artStyles.rotate}
           style={{
             background: "black",
-            width: 30,
-            height: 30,
+            color: "white",
+            width: 45,
+            height: 45,
             position: "absolute",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            bottom: 0,
+            right: 0,
           }}
-        />
+        >
+          <span style={{ fontSize: 9 }}>❤</span>
+          <span style={{ fontSize: 16 }}>☮</span>
+          <span style={{ fontSize: 9 }}>🤚</span>
+        </div>
 
         <Donut noSprinkle canvasBackground={canvasBackground} />
         <Donut
